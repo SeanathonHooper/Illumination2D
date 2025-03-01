92b89b2bfc23b7182696ecc08d6e0c914fcd7148
Sean Hoops <50555459+SeanathonHooper@users.noreply.github.com>
2025-03-01 18:34:43 -0500

    Various Changes
    - Added a Game Manager to automatically instantiate a camera and player entity at start of scene
    - Made Player rigidbody interpolate to stop jittering

---
+++ Assets/GameManager.cs
@@ -0,0 +1,19 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   [SerializeField] private GameObject playerPrefab;
+   [SerializeField] private GameObject cameraPrefab;
+   [SerializeField] private Transform levelStartPoint;
+
+   private CinemachineCamera _levelCam;
+   private PlayerState _playerEntity;
+
+   private void Awake()
+   {
+      _playerEntity = Instantiate(playerPrefab, levelStartPoint.position, levelStartPoint.rotation).GetComponent<PlayerState>();
+      _levelCam = Instantiate(cameraPrefab, new Vector3(0,0,-10), levelStartPoint.rotation).GetComponent<CinemachineCamera>();
+      _levelCam.Follow = _playerEntity.transform;
+   }
+}
